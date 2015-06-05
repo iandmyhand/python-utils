30ac63d485d548241e586b2698a10123b2a3cad9
SeomGi, Han <iandmyhand@gmail.com>
2015-06-05 09:48:46 +0900

    Add some python test codes.

---
+++ DataStructuresAndAlgorithmsInPython/Chapter01.PythonPrimer/PreviewOfAPythonProgram.py
@@ -0,0 +1,17 @@
+print("\nTest quotations.")
+print("Welcome to the GPA calculator.");
+print("""Welcome to the GPA calculator.
+Please enter all your letter grades, one per line.
+Enter a blank line to designate the end.""");
+
+print("\nPairs.");
+pairs = [('ga','Irish'), ('de','German')];
+print(pairs[0]);
+
+print("\nThe modulo operator.");
+print("27 // 4 =");
+print(27 // 4);
+
+print("\nInteger division.");
+print("27 % 4 =");
+print(27 % 4);

c56a40466a6196790aa0f95e4c6a9324355d8fdc
SeomGi, Han <iandmyhand@gmail.com>
2015-11-10 10:45:04 +0900

    Update structure

--- numberutils.py
+++ numberutils.py
@@ -1,8 +1,8 @@
-##-*- coding: utf-8 -*- 
+##-*- coding: utf-8 -*-
 #!/usr/bin/python
-'''
+"""
 Number to Hangul string util.
-'''
+"""
 
 __author__ = 'SeomGi, Han'
 __credits__ = ['SeomGi, Han']
@@ -28,25 +28,34 @@ HANGUL_NUMBER_BIG_UNIT = [
 
 class NumberUtils:
 
-    def __init__(self):
-        pass
+    _int_value = 0
+    _string_value = ''
+    _len_string_value = 0
 
-    def convert_number_to_hangul_string(self, number_value):
+    def __init__(self, int_value=0):
+        self._int_value = int_value
+        self._string_value = str(self._int_value)
+        self._len_string_value = len(self._string_value)
+
+    def convert_to_hangul_string(self):
+        """
+        usage:
+            numberutils.NumberUtils(220000112490).convert_to_hangul_string() == '이천이백억일십일만이천사백구십'
+        """
+        
         result = ''
-        string_value = str(number_value)
-        len_string_value = len(string_value)
-        if string_value and string_value.isdigit():
+        if self._string_value and self._string_value.isdigit():
             index = 0
-            while index < len_string_value:
+            while index < self._len_string_value:
                 single_result = ''
-                hangul_number = HANGUL_NUMBER[int(string_value[index])]
+                hangul_number = HANGUL_NUMBER[int(self._string_value[index])]
                 if hangul_number:
-                    unitindex = ((len_string_value - index) % 4) - 1
-                    single_result += hangul_number + HANGUL_NUMBER_UNIT[unitindex]
-                if (len_string_value - index - 1) % 4 == 0:
-                    big_unitindex = (len(string_value) - index - 1) // 4
-                    if len(HANGUL_NUMBER_BIG_UNIT) > big_unitindex:
-                        single_result += HANGUL_NUMBER_BIG_UNIT[big_unitindex]
+                    unit_index = ((self._len_string_value - index) % 4) - 1
+                    single_result += hangul_number + HANGUL_NUMBER_UNIT[unit_index]
+                if (self._len_string_value - index - 1) % 4 == 0:
+                    big_unit_index = (self._len_string_value - index - 1) // 4
+                    if len(HANGUL_NUMBER_BIG_UNIT) > big_unit_index:
+                        single_result += HANGUL_NUMBER_BIG_UNIT[big_unit_index]
                 result += single_result
                 index += 1
 

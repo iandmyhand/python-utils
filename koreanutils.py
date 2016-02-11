3c41f8e2f7588a460ba527d110c163f26bc9cc02
SeomGi, Han <iandmyhand@gmail.com>
2016-02-11 12:49:29 +0900

    Working on is_valid_korean_ssn function...

--- koreanutils.py
+++ koreanutils.py
@@ -30,9 +30,25 @@ class KoreanUtils:
         """
         usage:
             KoreanUtils().is_valid_korean_ssn('1234561234567') returns false
+        refernece:
+            https://github.com/kenu/egov/blob/master/ok.commoncomp/src/main/java/egovframework/com/utl/fcc/service/EgovNumberCheckUtil.java
         """
+
+        # 입력값이 숫자인지 확인
+        if not ssn.isdigit():
+            return False
+
+        # 주민등록번호 자리수 확인
         if 13 != len(ssn):
             return False
+
+        id_add = "234567892345"  # 주민등록번호에 가산할 값
+        count_num = 0
+        add_num = 0
+        total_id = 0  # 검증을 위한 변수선언
+
+        for index in range(len(ssn)):
+            print(str(index))
 
         return True
 

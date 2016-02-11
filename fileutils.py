82d90487a43e309074e5572b6ac529a707345274
SeomGi, Han <iandmyhand@gmail.com>
2016-02-11 11:49:36 +0900

    Add wrapped raw file copy function that use BufferedReader. And add logic to make directory if target directory isn't exist.

--- fileutils.py
+++ fileutils.py
@@ -3,7 +3,8 @@
 """
 Utilities related to Files.
 """
-from io import FileIO, BufferedWriter
+import os
+from io import FileIO, BufferedReader, BufferedWriter
 
 __author__ = 'SeomGi, Han'
 __credits__ = ['SeomGi, Han']
@@ -18,7 +19,11 @@ __status__ = 'Production'
 
 class FileUtils:
 
+    def copy_file_stream_to_file(self, file, to_path):
+        self.copy_buffered_io_to_file(BufferedReader(file), to_path)
+
     def copy_buffered_io_to_file(self, buffered_io, file_path):
+        os.makedirs(file_path[:file_path.rfind('/') + 1], exist_ok=True)
         with FileIO(file_path, mode='wb') as raw_output_io:
             with BufferedWriter(raw_output_io) as writer:
                 while 1:

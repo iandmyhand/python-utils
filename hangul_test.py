3b519b6ce6319797ef0544ea2567e918fa4df1b3
SeomGi, Han <iandmyhand@gmail.com>
2016-03-22 09:47:09 +0900

    Add test module of hangul.

---
+++ hangul_test.py
@@ -0,0 +1,14 @@
+﻿import hangul
+
+s = 'ㅎㅏㄴㅅㅓㅁㄱㅣ'
+print(hangul.conjoin(s))
+
+s = '한섬기'
+print(hangul.conjoin(s))
+
+print(ord('ㅎ'))
+print(ord(u'\u1112'))
+print(chr(12622))
+print(chr(4370))
+print(hex(12622))
+print(hex(4370))

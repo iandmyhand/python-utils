e100b037cce596e2a9230325a06d279e8b8f22cb
SeomGi, Han <iandmyhand@gmail.com>
2016-03-29 09:36:42 +0900

    Add exclude_extension function and some test cases.

--- stringutils.py
+++ stringutils.py
@@ -3,13 +3,14 @@
 """
 Utilities related to String.
 """
+import unittest
 
 __author__ = 'SeomGi, Han'
 __credits__ = ['SeomGi, Han']
 __copyright__ = 'Copyright 2015 SeomGi, Han, Python Utils Project'
 
 __license__ = 'MIT'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'SeomGi, Han'
 __email__ = 'iandmyhand@gmail.com'
 __status__ = 'Production'
@@ -20,6 +21,24 @@ class NotSupportedException(Exception):
     def __init__(self, message, errors):
         super(NotSupportedException, self).__init__(message)
         self.errors = errors
+
+
+class TestStringUtils(unittest.TestCase):
+
+    def test_extract_file_name(self):
+        self.assertTrue('filename.py' == StringUtils('/path/to/filename.py').extract_file_name())
+
+    def test_extract_extension(self):
+        self.assertTrue('py' == StringUtils('/path/to/filename.py').extract_extension())
+
+    def test_exclude_extension(self):
+        self.assertTrue('filename' == StringUtils('filename.py').exclude_extension())
+
+    def test_extract_directory(self):
+        self.assertTrue('/path/to/' == StringUtils('/path/to/filename.py').extract_directory())
+
+    def test_get_content_type(self):
+        self.assertTrue('image/png' == StringUtils('filename.png').get_content_type())
 
 
 class StringUtils:
@@ -55,6 +74,18 @@ class StringUtils:
         arr = self._string_value.split('.')
         extension = arr[len(arr) - 1]
         return extension
+    
+    def exclude_extension(self):
+        """
+        usage:
+            StringUtils('filename.py').exclude_extension() returns 'filename'
+        """
+        extension = self.extract_extension()
+        if not extension:
+            return None
+        arr = self._string_value.split(extension)
+        filename = arr[0][:-1]
+        return filename
 
     def extract_directory(self):
         """
@@ -89,3 +120,7 @@ class StringUtils:
         raise NotSupportedException(
             message='This extension[' + str(extension) + '] is not supported.',
             errors={'code': 'NOT_SUPPORTED_EXTENSION'})
+
+
+if __name__ == '__main__':
+    unittest.main()

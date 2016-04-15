042c11e298dd76c16ef84b2ee1d96d75de6203d4
SeomGi, Han <iandmyhand@gmail.com>
2016-04-15 18:16:21 +0900

    Print stack trace of exception.

--- print_traceback.py
+++ print_traceback.py
@@ -5,12 +5,14 @@ class CustomException(Exception):
     def __init__(self, *args, **kwargs):
         super(CustomException, self).__init__(*args, **kwargs)
 
-def custom_function():
+def custom_deep_function():
 	raise CustomException('Test to raise custom exception.')
+
+def custom_function():
+	custom_deep_function()
 
 try:
     custom_function()
 except:
     _type, _value, _traceback = sys.exc_info()
-    for _entry in traceback.format_tb(_traceback):
-    	print(_entry)
+    print(''.join(str(entry) for entry in traceback.format_tb(_traceback, limit=10)))
